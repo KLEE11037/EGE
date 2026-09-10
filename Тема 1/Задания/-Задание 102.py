e62975dd95583a113e5fe674916b83ee17d58df9
Кира <zakiraza2009@yandex.ru>
2026-09-10 18:54:31 +0300

    Обновлен статус задания № 102 Тема: 1 > Неверно

---
+++ Тема 1/Задания/-Задание 102.py
@@ -0,0 +1,14 @@
+# Решение
+
+
+
+
+
+answer = 9
+
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(1, 102, answer, 'aab3238922bcc25a6f606eb525ffdc56'))
