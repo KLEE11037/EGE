5e1258f6c0171cddb6d7403bf89fa74d4620924b
Кира <zakiraza2009@yandex.ru>
2026-09-20 14:22:09 +0300

    Обновлен статус задания № 204 Тема: 2 > Верно

---
+++ Тема 2/Задания/+Задание 204.py
@@ -0,0 +1,22 @@
+print('x y z w f')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                f = ((x<=y) or (z<=w)) and ((z==y)<=(w==x))
+                if f ==0:
+                    print(x,y,z,w,int(f))# Решение
+
+
+
+
+
+
+
+answer = 'yxwz'
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(2, 204, answer, '1ed5bb3720986c091b8dc2704366e53d'))
